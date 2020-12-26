d52edbc1c9a5b232322d70109dcd668091d04abf
kceron <karemiceron@gmail.com>
2020-12-26 14:25:18 -0500

    View Functions: State => UI

--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const PlayNumber = props => (
-    <button className="number" onClick={() => console.log(props.number)}>
+    
+    <button 
+        className="number" 
+        style={{ backgroundColor: props.colors[props.status] }}
+        onClick={() => console.log(props.number)}
+    >
         {props.number}
     </button>
 );
--- src/components/StarMatch.js
+++ src/components/StarMatch.js
@@ -44,6 +44,20 @@ const StarMatch = () => {
   };
 
   const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+  const numberStatus = (number) => {
+    if (!availableNums.includes(number)){
+      return 'used';
+    }
+    if (candidateNums.includes(number)){
+      return candidatesAreWrong ? 'wrong' : 'candidate';
+    }
+    return 'available';
+  };
     return (
       <div className="game">
         <div className="help">
@@ -55,7 +69,12 @@ const StarMatch = () => {
           </div>
           <div className="right">
             {utils.range(1, 9).map(number => 
-              <PlayNumber key={number} number={number} />
+              <PlayNumber 
+                colors={colors}
+                key={number} 
+                number={number} 
+                status={numberStatus(number)}
+              />
             )}
 
           </div>
