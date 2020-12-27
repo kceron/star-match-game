038d3a2e6965cbf54d7b34f483f2f702f6268674
kceron <karemiceron@gmail.com>
2020-12-27 00:26:42 -0500

    custom hooks

--- src/components/Game.js
+++ src/components/Game.js
@@ -42,21 +42,50 @@ import PlayAgain from './PlayAgain';
     },
   };
 
-const Game = (props) => {
-  const [stars, setStars] = useState(utils.random(1, 9));
-  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
-  const [candidateNums, setCandidateNums] = useState([]);
-  const [secondsLeft, setSecondsLeft] = useState(10);
+  // CUSTOM HOOK/stateful function
+  // rule of hooks: dont call hooks inside loops or conditions, cant conditionally use hooks / custum hooks start with the word use
+  // we define costum hooks to group everything thats about managing the state and transacting on the state
+  const useGameState = () => {
+    const [stars, setStars] = useState(utils.random(1, 9));
+    const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+    const [candidateNums, setCandidateNums] = useState([]);
+    const [secondsLeft, setSecondsLeft] = useState(10);
 
-  useEffect(() => {
-    if (secondsLeft > 0 && availableNums.length > 0){
-      const timerId = setTimeout(() => {
-        setSecondsLeft(secondsLeft - 1);
-      }, 1000);
-      // CLEAN UP EFFECT. The setTimeout function gives back a timerId which we can pass to clearTimeout to clean it up/remove the timer. ALWAYS CLEAN UP AFTER SIDE EFFECTS. If we introduce an effect, we should always return a function that cleans it up.
-      return () => clearTimeout(timerId);
-    }
-  });
+    useEffect(() => {
+      if (secondsLeft > 0 && availableNums.length > 0){
+        const timerId = setTimeout(() => {
+          setSecondsLeft(secondsLeft - 1);
+        }, 1000);
+        // CLEAN UP EFFECT. The setTimeout function gives back a timerId which we can pass to clearTimeout to clean it up/remove the timer. ALWAYS CLEAN UP AFTER SIDE EFFECTS. If we introduce an effect, we should always return a function that cleans it up.
+        return () => clearTimeout(timerId);
+      }
+    });    
+    // transactions on state
+    const setGameState = (newCandidateNums) => {
+      if (utils.sum(newCandidateNums) !== stars){
+        setCandidateNums(newCandidateNums);
+      }else {
+        const newAvailableNums = availableNums.filter(
+          n => !newCandidateNums.includes(n)
+        )
+        setStars(utils.randomSumIn(newAvailableNums, 9)); // redraw stars from whats avail
+        setAvailableNums(newAvailableNums);
+        setCandidateNums([]);
+      }
+    };
+      // return value could be anything: single value, obj 
+      // we return the things that the game component needs
+      return { stars, availableNums, candidateNums, secondsLeft, setGameState };
+  };
+
+const Game = (props) => {
+  const {
+    stars, 
+    availableNums,
+    candidateNums,
+    secondsLeft,
+    setGameState
+  } = useGameState();
 
   const candidatesAreWrong = utils.sum(candidateNums) > stars;
   const gameStatus = availableNums.length === 0 
@@ -82,23 +111,15 @@ const Game = (props) => {
   const onNumberClick = (number, currentStatus) => {
     if (gameStatus !== 'active' || currentStatus === 'used'){
       return;
-    }
-    const newCandidateNums = 
-      currentStatus === 'available'
-      ? candidateNums.concat(number)
-      : candidateNums.filter(cn => cn !== number);
+    };
 
-    if (utils.sum(newCandidateNums) !== stars){
-      setCandidateNums(newCandidateNums);
-    }else {
-      const newAvailableNums = availableNums.filter(
-        n => !newCandidateNums.includes(n)
-      )
-      setStars(utils.randomSumIn(newAvailableNums, 9)); // redraw stars from whats avail
-      setAvailableNums(newAvailableNums);
-      setCandidateNums([]);
-    }
-  }
+  const newCandidateNums = 
+    currentStatus === 'available'
+    ? candidateNums.concat(number)
+    : candidateNums.filter(cn => cn !== number);
+
+    setGameState(newCandidateNums);
+  };
 
     return (
       <div className="game">
